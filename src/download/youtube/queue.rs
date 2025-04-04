8f45bf1320ffc2a3e0d8ed3e7c7c6ce3c24e6426
Nathan Mossaad <git@nathanmossaad.com>
2025-04-05 00:18:00 +0200

    Added exception to not download youtube playlists

--- src/download/youtube/queue.rs
+++ src/download/youtube/queue.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use futures::future::join_all;
 use regex::Regex;
 use tokio::task::JoinHandle;
+use tracing::warn;
 
 use super::*;
 
@@ -13,10 +14,8 @@ pub struct YoutubeDownloadQueue {
 impl Config {
     async fn download_thread(&self) {
         while let Ok(vid) = self.youtube_queue.receiver.recv().await {
-            if let Err(e) = self
-                .direct_download_youtube(&vid.url, &vid.output)
-                .await
-            {
+            if let Err(e) = self.direct_download_youtube(&vid.url, &vid.output).await {
+                tracing::trace!("Failed download: {:#?}", e.to_string());
                 let context = format!("Failed to download video {}", vid.url.as_str());
                 self.status_bar
                     .register_err(&e.context(context).to_string())
@@ -55,7 +54,9 @@ impl Config {
         output_folder: PathBuf,
     ) -> Result<()> {
         // Regex adapted from: https://stackoverflow.com/questions/19377262/regex-for-youtube-url
-        let re = Regex::new(r#"(?:https?:?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?"#)?;
+        let re = Regex::new(
+            r#"(?:https?:?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?"#,
+        )?;
         // Iterate over every match in the search_space.
         for cap in re.captures_iter(search_space) {
             if let Some(url_str) = cap.get(0) {
@@ -63,11 +64,16 @@ impl Config {
 
                 // Validate and parse the url into the Url struct.
                 if let Ok(parsed_url) = Url::parse(url_value) {
-                    tracing::trace!("Invalid URL: {:?}", &parsed_url);
+                    if parsed_url.path().contains("playlist") {
+                        warn!("Currently youtube playlists are not supported, you may manually download the playlist \"{}\" to: \"{}\"", url_value, &output_folder.to_str().unwrap_or("Unknown Path"));
+                        self.status_bar.register_skipped().await;
+                        return Ok(())
+                    }
+                    tracing::trace!("Valid URL: {:?}", &parsed_url);
                     // Clone output_folder for each async call.
-                    self.queue_youtube_video(parsed_url, OutputType::Folder(output_folder.clone())).await?;
+                    self.queue_youtube_video(parsed_url, OutputType::Folder(output_folder.clone()))
+                        .await?;
                 } else {
-                    
                 }
             }
         }
